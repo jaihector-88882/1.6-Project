19451c79d5e3ee72bbbb6c1a60cfdbd3ee7b8276
jaihector-88882 <jaiden.aheath@gmail.com>
2026-03-18 08:37:19 -0400

    Add files via upload

---
+++ 1.6 Project.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+print("JaiHea8080's Ecel Spreadsheet Automation Menu")
+print("Choose a number from the following options")
+print("1. Input Data")
+print("2. View Current Data")
+print("3. Generate Report")
+
+choice = input(1)
+
+print("You selected 1 at 2026-10-30 17:17:31.159526")
